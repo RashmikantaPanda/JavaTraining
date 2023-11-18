98a8cc55ac74fa3f9a47de71b07d592e2c41bdce
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2023-11-18 11:04:13 +0530

    Logger example (log4j)

---
+++ Rk-IntellijWorkspace/log4j-example/src/main/java/org/oupp/training/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.oupp.training;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+public class HelloWorld {
+    static Logger logger = Logger.getLogger(HelloWorld.class);
+
+    public static void main(String[] args) {
+        DOMConfigurator.configure("src/main/resources/log4j.xml");
+        logger.debug("Sample debug message");
+        logger.info("Sample info message");
+        logger.warn("Sample warn message");
+        logger.error("Sample error message");
+        logger.fatal("Sample fatal message");
+    }
+}
