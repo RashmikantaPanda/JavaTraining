1aa90b059391a309ebaace3dbb04af6aeeb9e31a
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2023-11-24 12:34:18 +0530

    Object Cloning

---
+++ Rk-IntellijWorkspace/JavaTrainingBySaratSir/src/gov/in/oupp/training/java/corejava/io/cloning/ShallowCloningTest.java
@@ -0,0 +1,21 @@
+package gov.in.oupp.training.java.corejava.io.cloning;
+
+public class ShallowCloningTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Address oriAddress=new Address("Samsundarpur","Kendrapara");
+        Student oriStudent=new Student(1,"RK Panda",oriAddress);
+
+        Student clonedStudent= (Student) oriStudent.clone();
+
+        System.out.println("Original : "+oriStudent);
+        System.out.println("Shallow Clone : "+clonedStudent);
+
+        /*Modify the address in clone*/
+        clonedStudent.getAddress().setCity("BBSR");
+        System.out.println("After changing city in clone object");
+
+        /*Here, Both original and clone are affected*/
+        System.out.println("Original : "+oriStudent);
+        System.out.println("Shallow Clone : "+clonedStudent);
+    }
+}
