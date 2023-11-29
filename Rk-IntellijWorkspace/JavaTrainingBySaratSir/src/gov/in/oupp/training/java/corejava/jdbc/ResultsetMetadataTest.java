6a2eae67ca7299b3ca071067427b53c0e157e9c0
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2023-11-29 12:55:21 +0530

    Transaction,Callable Statement, ResultSetMetadata

---
+++ Rk-IntellijWorkspace/JavaTrainingBySaratSir/src/gov/in/oupp/training/java/corejava/jdbc/ResultsetMetadataTest.java
@@ -0,0 +1,28 @@
+package gov.in.oupp.training.java.corejava.jdbc;
+
+import java.sql.*;
+
+public class ResultsetMetadataTest {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/ouppdb";
+        String username = "root";
+        String password = "root";
+
+        try {
+            Connection con= DriverManager.getConnection(url,username,password);
+            System.out.println("Connection Established");
+
+            PreparedStatement ps=con.prepareStatement("select * from student ;");
+            ResultSet rs=ps.executeQuery();
+            ResultSetMetaData rsmd=rs.getMetaData();
+
+            System.out.println("Total Colums : "+rsmd.getColumnCount());
+            System.out.println("1st column name: "+rsmd.getColumnName(1)+"  datatype: "+rsmd.getColumnTypeName(1));
+            System.out.println("2nd column name: "+rsmd.getColumnName(2)+"  datatype: "+rsmd.getColumnTypeName(2));
+            System.out.println("3rd column name: "+rsmd.getColumnName(3)+"  datatype: "+rsmd.getColumnTypeName(3));
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
